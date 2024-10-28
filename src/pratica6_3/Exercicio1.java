e54ca68ad1299e5ec83ab41a3092da342cc60d03
CaciaRodrigues <caciarodriguues@gmail.com>
2024-10-28 15:13:16 -0300

    exercicio 01

---
+++ src/pratica6_3/Exercicio1.java
@@ -0,0 +1,27 @@
+package pratica6_3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		ler.useLocale(Locale.US); // Para a variável aceitar .00
+		
+		float salario;
+		float abono;
+		
+		System.out.printf("Diginte o Salário: ");
+		salario = ler.nextFloat();
+		
+		System.out.printf("Digite o Abono: ");
+		abono = ler.nextFloat();
+		
+		float novoSalario = salario + abono;
+		
+		System.out.printf("Novo Salário: R$%.2f", novoSalario);
+		
+	}
+}
