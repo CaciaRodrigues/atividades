b0d1240b66f9376a134bff10c11de2310b1aeead
CaciaRodrigues <caciarodriguues@gmail.com>
2024-10-31 17:41:08 -0300

    Exercício de Arrays 02

--- src/pratica9_7/Arrays01.java
+++ src/pratica9_7/Arrays01.java
@@ -1,9 +1,37 @@
 package pratica9_7;
 
+import java.util.Scanner;
+
 public class Arrays01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Scanner leitor = new Scanner(System.in);
+		
+		int listaNum[] = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+		
+		System.out.println("Digite o número que você deseja encontrar: ");
+		int numDesejo = leitor.nextInt();
+		
+		boolean achado = false;
+		int index = 0;
+		
+		for (int i = 0; i < listaNum.length; i++) {
+			if (listaNum[i] == numDesejo) {
+				achado = true;
+				index = i;
+				break;
+			}
+		}
+		
+		if(!achado) {
+			System.out.printf("O número %d não foi encontrado!", numDesejo);
+		}
+		else {
+			System.out.printf("O número %d está localizado na posição: %d", numDesejo, index);
+			
+		}
+		
+		leitor.close();
 
 	}
 
---
+++ src/pratica9_7/Arrays02.java
@@ -0,0 +1,50 @@
+package pratica9_7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arrays02 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int tamanho = 10; // declarando uma variável para ser utilizada repetidamente
+		int listaNum[] = new int[tamanho];
+		int soma = 0;
+		
+		System.out.println("-+-+-Monte um vetor com 10 números-+-+-\n");
+		
+		for (int i = 0; i < tamanho; i++) {
+			System.out.printf("Digite o %d° elemento: ", i + 1);
+			listaNum[i] = leitor.nextInt();
+			soma += listaNum[i];
+		}
+		
+		// Convertendo a variável tamanho de int para float somente na instância necessária
+		float media = soma / (float) tamanho;
+		
+		System.out.printf("O seu vetor completo: \n%s",Arrays.toString(listaNum));
+		
+		System.out.println("\nElementos nos índices ímpares: ");
+		for(int i = 0; i < tamanho; i++) {
+			if (i % 2 != 0) {
+				System.out.printf("%d ", listaNum[i]);
+			}
+		}
+		
+		System.out.println("\nElementos pares: ");
+		for(int j = 0; j < tamanho; j++) {
+			if(listaNum[j] % 2 == 0) {
+				System.out.printf("%d ", listaNum[j]);
+			}
+		}
+		
+		System.out.printf("\nSoma: %d"
+				        + "\nMédia: %.2f", soma, media);
+		
+		
+		leitor.close();
+
+	}
+
+}
