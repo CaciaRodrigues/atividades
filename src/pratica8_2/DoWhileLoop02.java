fc320a639360ccde26e6a79f5664e0aba0c180f8
CaciaRodrigues <caciarodriguues@gmail.com>
2024-10-31 18:49:33 -0300

    Exercício de laço de repetição Do...While 02

--- src/pratica8_2/DoWhileLoop02.java
+++ src/pratica8_2/DoWhileLoop02.java
@@ -6,6 +6,27 @@ public class DoWhileLoop02 {
 
 	public static void main(String[] args) {
 		
+		Scanner leitor = new Scanner(System.in);
+		
+		int numero = 0;
+		int soma = 0;
+		int contador = 0;
+		
+		do {
+			System.out.println("Digite um número: ");
+			numero = leitor.nextInt();
+			
+			if (numero % 3 == 0 && numero != 0) {
+				soma += numero;
+				contador++;
+			}
+		} while (numero !=0);
+		
+		float media = soma / (float) contador;
+		
+		System.out.printf("A média de todos os múltiplos de 3 é: %.1f", media);
+		
+		leitor.close();
 
 	}
 
